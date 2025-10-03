dfe30eb10b7b7186fd752a7997684598e810e67e
Odule Ayanfeoluwa Grandeur <95624629+ogayanfe@users.noreply.github.com>
2025-10-03 13:18:05 +0100

    style: update styling to make footer look okay on mobil e

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="w-full h-22 border-t-2 border-dashed border-gray-500 flex items-center justify-center font-black font-mono text-xl">
+    <div className="w-full h-22 border-t-2 border-dashed border-gray-500 flex items-center justify-center font-black font-mono text-xl text-center">
       <p>Copyright © 2025 — About The Authors — </p>
     </div>
   );
