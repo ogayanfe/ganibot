b7cce9ddc0d50756533e09cbcf70f69f83ecf419
mide2020-16 <efuwapeayomide51@gmail.com>
2025-10-06 14:52:03 +0100

    feat: added a new signin option, google

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
@@ -7,7 +8,31 @@ const handler = NextAuth({
       clientId: process.env.AUTH_GITHUB_ID as string,
       clientSecret: process.env.AUTH_GITHUB_SECRET as string,
     }),
+
+    GoogleProvider({
+      clientId: process.env.ATUH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
+
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google") {
+        const googleProfile = profile as any;
+        return googleProfile?.email_verified && googleProfile?.email?.endsWith("@example.com");
+      }
+      return true;
+    },
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
