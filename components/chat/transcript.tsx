35cb589a92cf8fd832e7fe6c10113f879a25001c
Odule Ayanfeoluwa Grandeur <95624629+ogayanfe@users.noreply.github.com>
2025-10-06 07:39:29 +0100

    feat:

--- components/chat/transcript.tsx
+++ components/chat/transcript.tsx
@@ -14,7 +14,7 @@ export default function Transcript({ loading }: IProps) {
     <div className="rounded-2xl p-6 flex flex-col justify-between max-w-xl flex-grow">
       <div className="space-y-4">
         <h2 className="text-xl font-semibold text-indigo-400">Transcription</h2>
-        <p className="text-lg md:text-2xl dark:text-gray-300 text-gray-700 font-mono">{transcripts}</p>
+        <p className="text-lg md:text-2xl dark:text-gray-300 text-gray-700 font-mono">{transcripts ?? "NOTHING YET"}</p>
         <ComponentVisiblity show={loading}>
           <div className="bg-gray-800 p-3 rounded-lg font-mono text-center text-yellow-300 text-sm break-words"></div>
           <div className="bg-gray-800 p-3 rounded-lg font-mono text-center text-yellow-300 text-sm break-words"></div>
