3dc86c25b9d4371787b01e9354944c820904103e
Odule Ayanfeoluwa Grandeur <95624629+ogayanfe@users.noreply.github.com>
2025-10-07 19:48:57 +0100

    feat: fix sign in page ui

--- app/(social)/(auth)/signin/page.tsx
+++ app/(social)/(auth)/signin/page.tsx
@@ -1,22 +1,20 @@
-'use client';
+"use client";
 
-import { signIn } from 'next-auth/react';
-import { FcGoogle } from 'react-icons/fc';
-import { FaGithub } from 'react-icons/fa';
+import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 export default function SignInPage() {
   return (
     <main className="flex items-center justify-center min-h-screen px-8 py-4">
-      <div className="w-full max-w-sm scale-200 shadow-sm border border-gray-200 dark:border-gray-900 rounded-2xl p-8 px-6 flex flex-col items-center overflow-hidden">
-        <h1 className="text-3xl font-semibold mb-8 text-center">
-          Sign in to your account
-        </h1>
+      <div className="w-full max-w-sm md:scale-120 shadow-sm border border-gray-200 dark:border-gray-900 rounded-2xl p-8 px-6 flex flex-col items-center overflow-hidden">
+        <h1 className="text-3xl font-semibold mb-8 text-center">Sign in to your account</h1>
 
         <div className="flex flex-col gap-4 w-full">
           <button
-            type='button'
-            title='Google SignIn'
-            onClick={() => signIn('google')}
+            type="button"
+            title="Google SignIn"
+            onClick={() => signIn("google")}
             className="flex items-center justify-center gap-3 border border-gray-300 rounded-lg py-2.5 hover:bg-gray-100 transition"
           >
             <FcGoogle size={20} />
@@ -24,9 +22,9 @@ export default function SignInPage() {
           </button>
 
           <button
-            type='button'
-            title='Github SignIn'
-            onClick={() => signIn('github')}
+            type="button"
+            title="Github SignIn"
+            onClick={() => signIn("github")}
             className="flex items-center justify-center gap-3 border border-gray-300 rounded-lg py-2.5 hover:bg-gray-100 transition"
           >
             <FaGithub size={20} />
@@ -35,7 +33,11 @@ export default function SignInPage() {
         </div>
 
         <p className="text-xs mt-6 text-center">
-          By signing in, you agree to our <a href="#" className='hover:underline hover:text-blue-500 transition-colors duration-300'>Terms & Privacy Policy</a>.
+          By signing in, you agree to our{" "}
+          <a href="#" className="hover:underline hover:text-blue-500 transition-colors duration-300">
+            Terms & Privacy Policy
+          </a>
+          .
         </p>
       </div>
     </main>
